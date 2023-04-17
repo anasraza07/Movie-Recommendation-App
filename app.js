79d650f3eee2ae2bf033aef3ce10cc281164fd44
anasraza07 <ar470684@gmail.com>
2023-04-17 07:26:42 +0500

    updated

--- app.js
+++ app.js
@@ -9,29 +9,80 @@
     const btnElem = document.getElementById("btn");
     const tableElem = document.getElementById("tableBody");
 
-    // function onLoad() {
-    movies.forEach(function (m) {
-        const optionElem = document.createElement("option")
-        for (var i = 0; i < m.genres.length; i++) {
-            optionElem.innerHTML = `${m.genres[i]}`;
+    // GENRES SETTING IN OPTIONS
+    function setOptions() {
+        let genresList = [];
+        movies.forEach(function (val) {
+            for (var i = 0; i < val.genres.length; i++) {
+                genresList.push(val.genres[i]);
+            }
+        });
+        // console.log(genresList)
+        const uniqueGenreList = [];
+        genresList.forEach(function (g) {
+            if (!uniqueGenreList.includes(g)) {
+                uniqueGenreList.push(g);
+            }
+        });
+
+        for (var i = 0; i < uniqueGenreList.length; i++) {
+            const optionElem = document.createElement("option");
+            optionElem.innerHTML = `${uniqueGenreList[i]}`;
+            genreElem.appendChild(optionElem);
         }
-        genreElem.appendChild(optionElem);
 
-        const optionElem1 = document.createElement("option")
-        optionElem1.innerHTML = `${new Date(m.release_date).getFullYear()}`;
-        yearElem.appendChild(optionElem1);
+        // YEAR SETTING IN OPTIONS
+        let yearList = [];
+        movies.forEach(function (val) {
+            yearList.push(new Date(val.release_date).getFullYear());
+        });
+        const uniqueYearList = [];
+        yearList.forEach(function (g) {
+            if (!uniqueYearList.includes(g)) {
+                uniqueYearList.push(g);
+            }
+        });
+        for (var i = 0; i < uniqueYearList.length; i++) {
+            const optionElem1 = document.createElement("option");
+            optionElem1.innerHTML = uniqueYearList[i];
+            yearElem.appendChild(optionElem1);
+        }
 
-        const optionElem2 = document.createElement("option")
-        optionElem2.innerHTML = `${m.original_language}`;
-        langElem.appendChild(optionElem2);
+        // LANG SETTING IN OPTIONS
+        let langList = [];
+        movies.forEach(function (val) {
+            langList.push(val.original_language);
+        });
+        const uniqueLangList = [];
+        langList.forEach(function (l) {
+            if (!uniqueLangList.includes(l)) {
+                uniqueLangList.push(l);
+            }
+        });
+        for (var i = 0; i < uniqueLangList.length; i++) {
+            const optionElem2 = document.createElement("option")
+            optionElem2.innerHTML = uniqueLangList[i]
+            langElem.appendChild(optionElem2);
+        }
 
-        const optionElem3 = document.createElement("option")
-        optionElem3.innerHTML = `${m.vote_average}`;
-        ratingElem.appendChild(optionElem3);
-
-    });
-    // }
-
+        // RATING SETTING IN OPTIONS
+        let ratingList = [];
+        movies.forEach(function (val) {
+            ratingList.push(val.vote_average);
+        });
+        const uniqueRatingList = [];
+        ratingList.forEach(function (r) {
+            if (!uniqueRatingList.includes(r)) {
+                uniqueRatingList.push(r);
+            }
+        });
+        for (var i = 0; i < uniqueRatingList.length; i++) {
+            const optionElem3 = document.createElement("option")
+            optionElem3.innerHTML = uniqueRatingList[i];
+            ratingElem.appendChild(optionElem3);
+        }
+    }
+    window.addEventListener("load", setOptions());
     function displayResults(results) {
         tableElem.innerHTML = ""
         results.forEach(function (movie, ind) {
