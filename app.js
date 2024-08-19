9921015c40c4e8b2f4c7bc86b91c3d2883c0660c
anasraza07 <ar470684@gmail.com>
2024-08-19 19:57:29 +0500

    responsiveness done and more extra features added

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
   const ratingElem = document.getElementById("select-rating");
   const moviesContainer = document.getElementById("movies-container");
 
-  // genres set
+  // set genre options
   let genresList = [];
   movies.forEach(function (movie) {
     for (var i = 0; i < movie.genres.length; i++) {
@@ -18,7 +18,7 @@
   });
   createOptions(genresList, genreElem);
 
-  // year set
+  // set year options
   let yearList = [];
   movies.forEach((movie) => {
     const fullYear = new Date(movie.release_date).getFullYear();
@@ -26,7 +26,7 @@
   });
   createOptions(yearList, yearElem);
 
-  // language set
+  // set language options
   let langList = [];
   movies.forEach(function (movie) {
     const language = movie.original_language;
@@ -34,13 +34,20 @@
   });
   createOptions(langList, langElem);
 
-  // rating set
+  // set rating options
   let ratingList = [];
-  movies.forEach(function (val) {
-    const roundedRating = Math.floor(val.vote_average);
-    cleanList(ratingList, roundedRating);
+  movies.forEach((movie) => {
+    const rating = movie.vote_average === 8 ? "8.0" : movie.vote_average;
+    cleanList(ratingList, rating);
   });
   createOptions(ratingList, ratingElem);
+
+  /*
+  sbse pehle chalta hai search function
+  aur scroll event pr handleScroll attach hojaata hai
+  phr search func loadMore ko call krta hai
+  loadMore displayResult ko call krta hai
+  */
 
   function createOptions(list, parentElem) {
     list.forEach((item) => {
@@ -60,75 +67,57 @@
     }
   }
 
-  const itemsPerPage = 10;
-  let page = 1;
   let postCount = 1;
-  let currentResults = [];
-
-  function displayResults(startIndex, endIndex) {
-    // moviesContainer.innerHTML = "";
+  function displayResult(startIndex, endIndex) {
     const resultsChunk = currentResults.slice(startIndex, endIndex);
-    console.log(resultsChunk);
-    if (resultsChunk.length === 0) {
-      console.log(resultsChunk.length);
-      moviesContainer.innerHTML = `
-                <div class="no-data">No Data</div>
-            `;
-      moviesContainer.style.margin = "10px 20px";
-      moviesContainer.style.display = "flex";
-      moviesContainer.style.alignItems = "center";
-      moviesContainer.style.justifyContent = "center";
-      moviesContainer.style.height = "35vh";
-      // return;
-    } else {
-      moviesContainer.style = "";
-    }
-    resultsChunk.forEach(function (movie, ind) {
-      // console.log(movieRow);
-      const movieRow = document.createElement("div");
-      // movieRow.classList = ["movie-row"];
-      // movie.classList = [""]
-      movieRow.setAttribute("id", "movie-row");
-      ind++;
-      movieRow.innerHTML = `
-                <div class="bg-red first flex hor-center" 
-                    id="rank">${postCount++}</div>
-                <div class="second" id="movie">
-                    <img src="https://image.tmdb.org/t/p/w45${
-                      movie.poster_path
-                    }" alt="Movie Image">
-                    <div id="description">
-                        <div id="title">${movie.title}</div>
-                        <div id="second-line">
-                            <span id="certification">${
-                              movie.certification || "Not available"
-                            }</span>
-                            <span id="genres">${movie.genres
-                              .sort()
-                              .join(", ")}</span>&#183 
-                            <span>${Math.floor(movie.runtime / 60)}h ${
-        movie.runtime % 60
-      }m</span>
-                        </div>
-                        </div>
-                        </div>
-                        <div class="bg-red third flex hor-center" id="year">${new Date(
-                          movie.release_date
-                        ).getFullYear()}</div>
-            `;
+    // console.log(resultsChunk);
 
-      // const hr = document.createElement("hr");
+    if (resultsChunk.length === 0) {
+      moviesContainer.innerHTML = `
+        <div class="no-data">No Data</div>`;
+      return;
+    }
+
+    resultsChunk.forEach((movie) => {
+      const movieRow = document.createElement("div");
+      movieRow.setAttribute("id", "movie-row");
+      movieRow.innerHTML = `
+                <div class="bg-red first flex hor-center" id="rank">
+                  ${postCount++}
+                </div>
+                <div class="second" id="movie">
+                  <img src="https://image.tmdb.org/t/p/w45${movie.poster_path}" alt="Movie Image">
+                  <div id="description">
+                    <div id="title">${movie.title}</div>
+                    <div id="second-line">
+                      <span id="certification">
+                        ${movie.certification || "Not available"}
+                      </span>
+                      <span id="genres">
+                      ${movie.genres.sort().join(", ")}
+                      </span>
+                      <span id="dot-entity">&#183;</span>
+                      <span id="movie-duration">${Math.floor(movie.runtime / 60)}h ${movie.runtime % 60}m
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="bg-red third flex hor-center" id="year">
+                  ${new Date(movie.release_date).getFullYear()}
+                </div>
+      `;
       moviesContainer.appendChild(movieRow);
-      // moviesContainer.appendChild(hr);
     });
   }
 
+  // pagination
+  let page = 1;
+  const itemsPerPage = 10;
   function loadMore() {
     const startIndex = (page - 1) * itemsPerPage;
     const endIndex = page * itemsPerPage;
 
-    displayResults(startIndex, endIndex);
-
+    displayResult(startIndex, endIndex);
     page++;
 
     if (endIndex >= currentResults.length || currentResults.length === 0) {
@@ -136,50 +125,42 @@
     }
   }
 
-  const handleScroll = () => {
+  window.addEventListener("scroll", handleScroll);
+  function handleScroll() {
     const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
     if (scrollTop + clientHeight >= scrollHeight) {
-      setTimeout(() => {
-        console.log("I am at bottom");
-        loadMore();
-      }, 200);
+      setTimeout(() => loadMore(), 300);
     }
   };
 
-  const initInfiniteScroll = () => {
-    window.addEventListener("scroll", handleScroll);
-  };
-
+  let currentResults = [];
   function search() {
     const genreQuery = genreElem.value;
     const yearQuery = yearElem.value;
     const langQuery = langElem.value;
     const ratingQuery = ratingElem.value;
 
-    currentResults = movies.filter(function (movie) {
+    currentResults = movies.filter((movie) => {
       return (
         (genreQuery === "all" ||
           movie.genres.toString().includes(genreQuery)) &&
         (yearQuery === "all" ||
           new Date(movie.release_date).getFullYear() == yearQuery) &&
         (langQuery === "all" || movie.original_language === langQuery) &&
-        (ratingQuery === "all" ||
-          movie.vote_average.toString().includes(ratingQuery))
+        (ratingQuery === "all" || movie.vote_average == ratingQuery)
       );
     });
-    // console.log("results", results)
-    // displayResults(results, startIndex, endIndex)
+    // console.log(currentResults);
+    document.querySelector('#js-search-results')
+      .innerHTML = `Movies: ${currentResults.length < 10 && currentResults.length !== 0 ? '0' + currentResults.length : currentResults.length}`;
+
     page = 1;
     postCount = 1;
     moviesContainer.innerHTML = "";
 
     loadMore();
-    initInfiniteScroll();
   }
   search();
-
-  initInfiniteScroll();
-  // loadMore();
 
   const allSelectInput = document.querySelectorAll(".user-select");
   allSelectInput.forEach((q) => {
